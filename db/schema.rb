06aa74c812dff78b179032c40a26d0218dd1e7eb
Daniel Trujillo <ganimedes@firemail.cc>
2020-04-05 17:45:31 -0500

    created movie model scaffold and configured routes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_05_222117) do
+ActiveRecord::Schema.define(version: 2020_04_05_223501) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,4 +22,19 @@ ActiveRecord::Schema.define(version: 2020_04_05_222117) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.float "rating"
+    t.string "director"
+    t.integer "year"
+    t.integer "duration"
+    t.text "synopsys"
+    t.string "url"
+    t.bigint "genre_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["genre_id"], name: "index_movies_on_genre_id"
+  end
+
+  add_foreign_key "movies", "genres"
 end

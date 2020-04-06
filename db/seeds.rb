f439b89284d56623ee523f609fe927785b5f8a29
Daniel Trujillo <ganimedes@firemail.cc>
2020-04-05 19:35:47 -0500

    created seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,36 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+Genre.destroy_all
+Movie.destroy_all
+
+puts 'Creating 10 fake genres...'
+
+genreList = ['Action', 'Adventure', 'Comedy', 'Crime', 'Drama', 'Fantasy',
+            'Historical', 'Horror', 'Mystery', 'Philosophical', 'Political',
+            'Romance', 'Satire', 'Sci-Fi', 'Thriller', 'Western']
+
+genreList.each do |item|
+  genreInstance = Genre.new(
+    name: item,
+    url: Faker::LoremFlickr.image(size: "300x200", search_terms: ['movies'])
+  )
+  genreInstance.save!
+
+  puts 'Succesfully created 10 genres'
+  puts "Creating 10 fake movies for #{item}"
+
+  10.times do
+    movieInstance = Movie.new(
+      title: Faker::Book.title,
+      rating: rand(2..5.0).round(1),
+      director: Faker::Name.name,
+      year: rand(1960..2020),
+      duration: rand(90..180),
+      synopsys: Faker::Lorem.paragraphs,
+      url: Faker::LoremFlickr.image(size: "300x200", search_terms: ['movies']),
+      genre_id: genreInstance.id
+    )
+    movieInstance.save!
+  end
+  puts 'Succesfully created 10 movies for #{item}'
+end
+puts 'Finished!'
